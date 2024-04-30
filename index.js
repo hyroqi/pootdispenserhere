5587622e61ac9e03133299bd341544d1c9096342
hyroqi <hiroq.i@outlook.com>
2024-04-30 12:13:12 +0800

    put dispenser here

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+
 let count = 0
 
 function increment() {
     count += 1
     countEl.textContent = count
+    let pootis = new Audio('putdispenser.mp3')
+    pootis.play()
+    var imageContainer = document.getElementById("imageContainer");
+    imageContainer.style.bottom = "0";
+    setTimeout(function() {
+        imageContainer.style.bottom = "-100%"; // Hide the image container
+      }, 1500); // Change 5000 to the desired delay in milliseconds
 }
 
 function save() {
@@ -12,4 +20,11 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    let built = new Audio('ibuiltthat.mp3')
+    built.play()
+    var imageContainer = document.getElementById("imageContainer2");
+    imageContainer.style.bottom = "0";
+    setTimeout(function() {
+        imageContainer.style.bottom = "-100%"; // Hide the image container
+      }, 1000); // Change 5000 to the
 }
